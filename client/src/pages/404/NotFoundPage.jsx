1c4842ca446dd486d5667e3ccccd016f518baddd
DStukalo <94228608+DStukalo@users.noreply.github.com>
2023-04-11 20:24:24 +0300

    feat: add not found page

---
+++ client/src/pages/404/NotFoundPage.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
+
+export function NotFoundPage() {
+	const [second, setSecond] = useState(5);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		document.title = '404 || Not Found';
+	}, []);
+
+	function tic() {
+		if (second > 0) setSecond(second - 1);
+	}
+
+	function updateTime() {
+		const timeId = setInterval(tic, 1000);
+		function redirectToMain() {
+			clearInterval(timeId);
+			navigate('/');
+		}
+		setTimeout(redirectToMain, 5000);
+	}
+	updateTime();
+
+	return (
+		<div className={styles.wrapper}>
+			<section className={styles.section}>
+				<div className={styles.text}>
+					This page Not Found.
+				</div>
+				<div className={styles.text}>
+					You will be redirected to the home page via
+					{' '}
+					<span className={styles.time}>{second}</span>
+				</div>
+			</section>
+		</div>
+	);
+}
